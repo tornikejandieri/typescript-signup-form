20120118ac4ad114e38a8c4f8e65dedf6336aa75
Tornike Jandieri <108745129+tornikejandieri@users.noreply.github.com>
2022-09-13 03:03:01 +0400

    added submit logic and list display

--- public/app.js
+++ public/app.js
@@ -1,1 +1,52 @@
 "use strict";
+const button = document.getElementById('add-data');
+const td = document.querySelector('td');
+let row = 1;
+button.addEventListener('click', (e) => {
+    const buttonHolder = document.createElement('div');
+    buttonHolder.setAttribute('class', '.newButtonHolder');
+    const deletebtn = document.createElement('button');
+    deletebtn.textContent = 'X';
+    deletebtn.style.backgroundColor = 'red';
+    deletebtn.style.color = 'white';
+    // prevent reloads when submitting
+    e.preventDefault();
+    // grab elements
+    let firstName = document.getElementById('firstname');
+    let lastName = document.getElementById('lastname');
+    let address = document.getElementById('address');
+    let birthday = document.getElementById('date');
+    let gender = document.getElementById('gender');
+    let notes = document.getElementById('notes');
+    // alert user if they forgot to input
+    if (!firstName.value || !lastName.value || !address.value) {
+        alert('Required field is empty');
+    }
+    let table = document.getElementById('display');
+    let newRow = table.insertRow(row);
+    // assign numbers to cells
+    const cellOne = newRow.insertCell(0);
+    const cellTwo = newRow.insertCell(1);
+    const cellThree = newRow.insertCell(2);
+    const cellFour = newRow.insertCell(3);
+    const cellFive = newRow.insertCell(4);
+    const cellSix = newRow.insertCell(5);
+    const cellSeven = newRow.insertCell(6);
+    //put the input values in the table
+    cellOne.innerHTML = firstName.value;
+    cellTwo.innerHTML = lastName.value;
+    cellThree.innerHTML = address.value;
+    cellFour.innerHTML = birthday.value;
+    cellFive.innerHTML = gender.value;
+    cellSix.innerHTML = notes.value;
+    //fix button bug
+    cellSeven.append(deletebtn);
+    row++;
+    // reset input fields after submitting
+    firstName.value = '';
+    lastName.value = '';
+    address.value = '';
+    birthday.value = '';
+    gender.value = '';
+    notes.value = '';
+});
---
+++ src/app.ts
@@ -0,0 +1,67 @@
+const button = document.getElementById('add-data')
+const td = document.querySelector('td')!
+
+
+let row = 1
+
+
+button!.addEventListener('click', (e) => {
+  const buttonHolder = document.createElement('div')
+  buttonHolder.setAttribute('class', '.newButtonHolder')
+
+  const deletebtn = document.createElement('button')
+  deletebtn.textContent = 'X'
+  deletebtn.style.backgroundColor = 'red'
+  deletebtn.style.color = 'white'
+
+
+
+          
+  // prevent reloads when submitting
+  e.preventDefault()
+ // grab elements
+  let firstName = document.getElementById('firstname') as HTMLInputElement
+  let lastName = document.getElementById('lastname') as HTMLInputElement
+  let address = document.getElementById('address') as HTMLInputElement
+  let birthday = document.getElementById('date') as HTMLInputElement
+  let gender = document.getElementById('gender') as HTMLOptionElement
+  let notes = document.getElementById('notes') as HTMLInputElement
+ // alert user if they forgot to input
+  if(!firstName.value || !lastName.value || !address.value){
+    alert('Required field is empty')
+  }
+
+let table = document.getElementById('display') as HTMLTableElement
+
+let newRow = table.insertRow(row) 
+// assign numbers to cells
+const cellOne: any = newRow.insertCell(0)
+const cellTwo: any = newRow.insertCell(1)
+const cellThree: any = newRow.insertCell(2)
+const cellFour: any = newRow.insertCell(3)
+const cellFive: any = newRow.insertCell(4)
+const cellSix: any = newRow.insertCell(5)
+const cellSeven: any = newRow.insertCell(6)
+
+
+//put the input values in the table
+cellOne.innerHTML = firstName.value
+cellTwo.innerHTML = lastName.value
+cellThree.innerHTML = address.value
+cellFour.innerHTML = birthday.value
+cellFive.innerHTML = gender.value
+cellSix.innerHTML = notes.value
+
+//fix button bug
+cellSeven.append(deletebtn)
+
+
+row++
+// reset input fields after submitting
+firstName.value = ''
+lastName.value = ''
+address.value = ''
+birthday.value = ''
+gender.value = ''
+notes.value = ''
+})
